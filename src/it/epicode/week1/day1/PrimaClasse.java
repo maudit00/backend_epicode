ccd2c3a87e5fbe6b7986da659868f563b594270d
Maudit00 <davide9311@gmail.com>
2024-01-08 14:23:30 +0100

    first method usage

--- src/it/epicode/week1/day1/PrimaClasse.java
+++ src/it/epicode/week1/day1/PrimaClasse.java
@@ -2,11 +2,13 @@ package it.epicode.week1.day1;
 
 public class PrimaClasse {
 	public static void main(String[] args) {
-	    int x = 3;
-	    boolean b = true;
-	    char c = 'c';
-	    byte a = 4;
 	    float f = 4.5f;
 	    System.out.println(f);
+	    double somma = somma(4,5);
+	    System.out.println(somma);
+	}
+	public static double somma(double x, double y){
+		double somma = x + y ;
+		return somma; 
 	}
 }
