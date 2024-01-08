bc252e08f1f0e97e33e18c57a45517f257bd173e
Maudit00 <davide9311@gmail.com>
2024-01-08 16:37:14 +0100

    esercizio 3, esercizio 2 done

--- src/it/epicode/week1/day1/ProvaArray.java
+++ src/it/epicode/week1/day1/ProvaArray.java
@@ -4,19 +4,23 @@ import java.util.Scanner;
 public class ProvaArray {
 
 	public static void main (String[] args){
+		// primo metodo per creare un array
 		Scanner scanner = new Scanner(System.in);
 		int[] numeri = new int[5];
-		numeri [1] = 3;
-		numeri [2] = 6;
-		numeri [3] = 9;
-
+		// ciclo per chiedere i numeri in input
 		for (int i = 0; i < numeri.length; i++){
 			System.out.println("Dammi un numero");
 			numeri[i] = scanner.nextInt();
 		}
+		//ciclo per stampare i numeri nell'array
 
 		for (int i = 0; i < numeri.length; i++){
 			System.out.println(numeri[i]);
+		}
+
+		String[] giorniSettimana = {"lunedì", "martedì" , "mercoledì", "giovedì", "venerdì", "sabato", "domenica"};
+		for (int i = 0; i < giorniSettimana.length; i++){
+			System.out.println(giorniSettimana[i]);
 		}
 	}
 }
