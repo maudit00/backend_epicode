55038f59843dfda64dbe58d78cdb6be66b984d13
Maudit00 <davide9311@gmail.com>
2024-01-08 12:39:45 +0100

    testing first java snippets

--- src/it/epicode/week1/day1/PrimaClasse.java
+++ src/it/epicode/week1/day1/PrimaClasse.java
@@ -1,4 +1,12 @@
 package it.epicode.week1.day1;
 
 public class PrimaClasse {
+	public static void main(String[] args) {
+	    int x = 3;
+	    boolean b = true;
+	    char c = 'c';
+	    byte a = 4;
+	    float f = 4.5f;
+	    System.out.println(f);
+	}
 }
