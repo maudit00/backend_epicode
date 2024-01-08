7e5502a5b7b978e346125cf59aca9b96df6d9571
Maudit00 <davide9311@gmail.com>
2024-01-08 14:51:40 +0100

    testing with methods

--- src/it/epicode/week1/day1/PrimaClasse.java
+++ src/it/epicode/week1/day1/PrimaClasse.java
@@ -1,14 +1,30 @@
 package it.epicode.week1.day1;
+import java.util.Scanner;
 
 public class PrimaClasse {
 	public static void main(String[] args) {
-	    float f = 4.5f;
-	    System.out.println(f);
-	    double somma = somma(4,5);
-	    System.out.println(somma);
+		//dichiarazione e assegnazione di una variabile
+		float f = 4.5f;
+		// stampa della variabile
+		System.out.println(f);
+		//creazione di una nuova variabile e assegnazione del risultato di un metodo esterno
+		double somma = somma(4,5);
+		System.out.println(somma);
+		//utilizzo di un metodo void con parametro statico
+		benvenuto("Davide");
+		//Chiedo in input il nome all'utente
+		System.out.println("Inserisci il tuo nome");
+		//creo un nuovo oggetto dalla classe scanner che bisogna importare per forza a meno che la classe sia nello stesso package
+		Scanner scanner = new Scanner(System.in);
+		String j = scanner.next();
+		benvenuto(j);
+
 	}
 	public static double somma(double x, double y){
 		double somma = x + y ;
 		return somma; 
+	}
+	public static void benvenuto(String nome) {
+		System.out.println("Benvenuto " + nome);
 	}
 }
