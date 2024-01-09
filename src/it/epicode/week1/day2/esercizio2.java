a586ce94427866bba7344eafc8ccc2581ec8c043
Maudit00 <davide9311@gmail.com>
2024-01-09 15:01:40 +0100

    esercizio 2 done

--- src/it/epicode/week1/day2/esercizio2.java
+++ src/it/epicode/week1/day2/esercizio2.java
@@ -1,7 +1,22 @@
 package it.epicode.week1.day2;
+import java.util.Scanner;
 
 public class esercizio2 { 
 	public static void main (String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Inserisci un numero tra 0 e 3");
+		int x = scanner.nextInt();
 
+		switch (x){
+			case 0 : System.out.println("Zero");
+				 break;
+			case 1 :  System.out.println("Uno");
+				 break;
+			case 2 :  System.out.println("Due");
+				 break;
+			case 3 :  System.out.println("Tre");
+				 break;
+			default : System.out.println("Non hai inserito un numero tra 0 e 3");
+		}
 	}
 }
