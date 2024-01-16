535a95656f98688e59a6323cb8b21a8b4bf759d7
Maudit00 <davide9311@gmail.com>
2024-01-16 16:48:34 +0100

    exercise 2 done

--- src/it/epicode/week2/day2/esercizio2.java
+++ src/it/epicode/week2/day2/esercizio2.java
@@ -1,5 +1,7 @@
 package it.epicode.week2.day2;
 
+import ch.qos.logback.core.encoder.JsonEscapeUtil;
+
 import java.util.*;
 
 public class esercizio2 {
@@ -14,15 +16,19 @@ public class esercizio2 {
 
         for (int i = 0; i < n ; i++){
             int randNum = randomNumber.nextInt(100) + 1 ;
-            System.out.println(randNum);
             lista.add(randNum);
         }
+
+        printList(lista);
+        printList(revertList(lista));
+        printOddEven(lista, true);
+        printOddEven(lista, false);
 
     }
 
     public static ArrayList<Integer> revertList(ArrayList<Integer> lista){
         ArrayList<Integer> revertedList = new ArrayList<>();
-        for (int i = (lista.size() -1); i == 0 ; i --){
+        for (int i = (lista.size() -1); i >= 0 ; i --){
           revertedList.add(lista.get(i));
         }
         return revertedList;
@@ -32,6 +38,24 @@ public class esercizio2 {
         System.out.println("Lista Stampata :");
         for (Integer n:list){
             System.out.println(n);
+        }
+    }
+
+    public static void printOddEven (ArrayList<Integer> list, boolean even) {
+        if (even) {
+            System.out.println("Numeri posizioni pari");
+            for (int i = 0; i < list.size(); i++) {
+                if (i % 2 == 0) {
+                    System.out.println(list.get(i));
+                }
+            }
+        } else {
+            System.out.println("Numeri posizioni dispari");
+            for (int i = 0; i < list.size(); i++) {
+                if (i % 2 != 0) {
+                    System.out.println(list.get(i));
+                }
+            }
         }
     }
 }
